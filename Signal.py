1cc4e4d14d52bbadeb38c5bf4fc67b0e6dfcfe8d
Farid <farid_farazi@ymail.com>
2022-09-22 07:07:17 +0330

    Analog signal class is completed."

--- Signal.py
+++ Signal.py
@@ -35,34 +35,36 @@ class Digital():
 
 class Analog():
     def __init__(self, id, name, type, address, bit_rate, value= 0): #, status
-        self.id = id
-        self.name = name
-        self.type = type
-        self.address = address
-        self.bit_rate = bit_rate
+        self.id = id                    #An ID has to be assigned to every input or output signal
+        self.name = name                #Every sensor has a name in an industrial plan
+        self.type = type                #Every AnalogIO has an electrical type(4 to 20 mA or 0 to 5 Volts or 0 to 10 Volts ...)
+        self.address = address          #IO cards has an address. it is just to save it for better and easier maintenance
+        self.bit_rate = bit_rate        #the Analog signal resolution
         #self.status =status
-        self.value = value
+        self.value = value              #Every AnalogIO has a value
         self.force = False
 
     def Signal_status(self):
             return {"Force":self.force , "Value":self.value} #"Status":self.status ,
 
     def Signal_information(self):
-        return {"ID":self.id, "Name":self.name, "Type":self.type, "Bit Rate":self.bit_rate,
-                "Status":self.status ,"Force":self.force ,"Value":self.value}
+        return {"ID":self.id, "Name":self.name, "Type":self.type, "Address":self.address, "Bit Rate":self.bit_rate,
+                "Force":self.force ,"Value":self.value} #"Status":self.status ,
     
     def Signal_update(self,signal_value):
         if not self.force:
             self.value = signal_value
         else: pass
 
-    def Force(self,address_value, signal_value, force=False):
+    def Signal_force(self,address_value, signal_value, force=False):
         if not force:
             self.Signal_update(address_value)
+            print("Update")
         elif force == True:
-            self.status = signal_value
+            self.value = signal_value
         self.force = force
+        
 
-    def Signal_scale(self, low, high):
-        m = (low - high)/ 2**self.bit_rate
-        return float (m(self.value)-low)
+    def Scale(self, Y0, Y1, X0, X1):
+        m = ( Y1 - Y0 )/ (X1 - X0)
+        return m*(self.value - X0 )+ Y0
